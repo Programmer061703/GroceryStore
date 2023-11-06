270f5d59d6acc09de8b1dafb380377fd91caa50a
Blake Williams <blaker1703@gmail.com>
2023-11-06 12:17:39 -0600

    first commit

---
+++ GroceryStore.java
@@ -0,0 +1,57 @@
+import java.util.concurrent.*;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class GroceryStore{
+
+    public static void main(String[] args){
+
+
+    }
+
+}
+
+ class Customer implements Runnable{
+
+        Customer(){
+
+
+        }
+
+        private void getWaitingRoom(){
+
+
+        }
+
+        private void getProduceSection(){
+
+        }
+
+        private void getGeneralSection(){
+
+        }
+
+        private void getFrozenSection(){
+
+        }
+
+        private void getCashierSectio(){
+
+        }
+
+        private void exitGroceryStore(){
+
+        }
+
+        public void run(){
+
+        }
+
+
+
+
+}
